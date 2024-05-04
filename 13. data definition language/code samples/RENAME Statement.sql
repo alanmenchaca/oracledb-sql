7e846cf52d5269ce64ba306073299ad80ca89e72
Fco. Alan Menchaca M. <alanmenchaca22@outlook.com>
2024-05-03 18:09:47 -0600

    data definition language (DDL)

---
+++ 13. data definition language/code samples/RENAME Statement.sql
@@ -0,0 +1,16 @@
+/************* The Complete Oracle SQL Bootcamp *************
+ * Author  : Oracle Master Training                         *
+ * Course  : The Complete Oracle SQL Bootcamp               *
+ * Lecture : RENAME Statement                               *
+ ************************************************************/
+
+DESC employees_copy;
+ALTER TABLE employees_copy RENAME COLUMN hire_date TO start_date;
+
+RENAME employees_copy TO employees_backup;
+
+SELECT * FROM employees_copy;
+SELECT * FROM employees_backup;
+
+ALTER TABLE employees_backup RENAME TO employees_copy;
+SELECT * FROM employees_copy;
